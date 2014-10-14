b09a4c7425fc3991eee1530c8d56e2d660026a93
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-10-13 21:41:07 -0400

    Added GNU copyright to primary plugin file.

--- nestedpages.php
+++ nestedpages.php
@@ -9,6 +9,23 @@ Author URI: https://github.com/kylephillips
 License: GPLv2 or later.
 Copyright: Kyle Phillips
 */
+
+/*  Copyright 2014 Kyle Phillips  (email : support@nestedpages.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 if( !class_exists('NestedPages') ) :
 	require_once('includes/class-nestedpages.php');
 	$nested_pages = new NestedPages;
