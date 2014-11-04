35a24af4d5429906fac10452f7ea8b83b8c2cefd
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-11-03 23:40:37 -0500

    Redirect to nested pages added for trashed pages

--- includes/class-np-redirects.php
+++ includes/class-np-redirects.php
@@ -4,9 +4,25 @@
 */
 class NP_Redirects {
 
+
 	public function __construct()
 	{
+		add_action('load-edit.php', array($this, 'pageTrashed'));
+	}
 
+
+	/**
+	* Redirect back to nested pages after a page is trashed
+	* @todo - provide alert indicator on trash success
+	*/
+	public function pageTrashed()
+	{
+		$screen = get_current_screen();
+		if ( ($screen->id == 'edit-page') && (isset($_GET['trashed'])) && (intval($_GET['trashed']) >0)){
+			$redirect = add_query_arg(array('page'=>'nestedpages', 'trashed' => false, 'ids' => $_GET['ids'] ));
+			wp_redirect($redirect);
+			exit();
+		}
 	}
 
 }
