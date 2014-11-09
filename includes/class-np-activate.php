f898829697bff336bcbdfbfe9d92e61133251c32
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-11-08 21:48:19 -0500

    Updated version numbers for release

--- includes/class-np-activate.php
+++ includes/class-np-activate.php
@@ -14,7 +14,7 @@ class NP_Activate {
 	public function __construct()
 	{
 		register_activation_hook( dirname( dirname(__FILE__) ) . '/nestedpages.php', array($this, 'install') );
-		$this->version = 1.1;
+		$this->version = '1.1.1';
 		$this->setVersion();
 		$this->addMenu();
 	}
