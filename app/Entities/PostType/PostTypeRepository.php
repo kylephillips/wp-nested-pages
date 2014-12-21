7259a74d07a9bd4e3905a9e29add22dca59fe350
Kyle Phillips <kylephillipsdesign@gmail.com>
2014-12-21 12:15:00 -0500

    Post type option saving complete.

--- app/Entities/PostType/PostTypeRepository.php
+++ app/Entities/PostType/PostTypeRepository.php
@@ -5,14 +5,14 @@ class PostTypeRepository {
 
 	/**
 	* Get Available Post Types
+	* @since 1.2.1
 	* @return array
 	*/
 	public function getPostTypes($return = 'names')
 	{
 		$args = array(
 			'public' => true,
-			'show_ui' => true,
-			'hierarchical' => true
+			'show_ui' => true
 		);
 		$types = get_post_types($args, $return);
 		return $types;
@@ -21,6 +21,7 @@ class PostTypeRepository {
 
 	/**
 	* Get an object of non-page post types
+	* @since 1.2.1
 	* @return object
 	*/
 	public function getPostTypesObject($pages = false)
@@ -28,14 +29,27 @@ class PostTypeRepository {
 		$all_types = $this->getPostTypes('objects');
 		$i = 0;
 		foreach($all_types as $key => $type){
-			if ( (!$pages) && ($key == 'page') ) continue;
+			if ( (!$pages) && ($key == 'page') || ($key == 'attachment') ) continue;
 			$post_types[$i] = new \stdClass();
 			$post_types[$i]->name = $type->name;
 			$post_types[$i]->label = $type->labels->name;
 			$post_types[$i]->hierarchical = $type->hierarchical;
+			$post_types[$i]->np_enabled = ( in_array($type->name, $this->enabledPostTypes()) ) ? true : false;
 			$i++;
 		}
 		return $post_types;
+	}
+
+
+	/**
+	* Get an array of NP enabled Post Types
+	* @since 1.2.1
+	* @return array
+	*/
+	public function enabledPostTypes()
+	{
+		$types = get_option('nestedpages_posttypes');
+		return $types;
 	}
 
 }
--- app/Views/settings-posttypes.php
+++ app/Views/settings-posttypes.php
@@ -4,7 +4,7 @@ settings_fields( 'nestedpages-posttypes' );
 ?>
 <tr valign="top">
 	<td colspan="2" style="padding:0px;">
-		<h3 style="margin-bottom:5px;"><?php _e('Enable Nested Pages for:', 'nestedpages'); ?></h3>
+		<h3 style="margin-bottom:10px;"><?php _e('Enable Nested Pages for:', 'nestedpages'); ?></h3>
 	</td>
 </tr>
 <tr valign="top">
@@ -12,8 +12,8 @@ settings_fields( 'nestedpages-posttypes' );
 		<table width="100%" class="nestedpages-settings-table">
 			<thead>
 				<th><?php _e('Post Type', 'nestedpages'); ?></th>
-				<th><?php _e('Hierarchical?', 'nestedpages'); ?></th>
-				<th><?php _e('Enable', 'nestedpages'); ?></th>
+				<th><?php _e('Hierarchical', 'nestedpages'); ?></th>
+				<th><?php _e('Enabled', 'nestedpages'); ?></th>
 			</thead>
 			<?php foreach ($types as $type) : ?>
 			<tr>
@@ -24,7 +24,7 @@ settings_fields( 'nestedpages-posttypes' );
 					<?php endif; ?>
 				</td>
 				<td>
-					<input type="checkbox" name="nestedpages_posttypes[]" value="<?php echo $type->name; ?>" />
+					<input type="checkbox" name="nestedpages_posttypes[]" value="<?php echo $type->name; ?>" <?php if ( $type->np_enabled ) echo 'checked'; ?> />
 				</td>
 			</tr>
 			<?php endforeach; ?>
@@ -32,12 +32,9 @@ settings_fields( 'nestedpages-posttypes' );
 	</td>
 </tr>
 <tr valign="top">
-	<td colspan="2" style="padding:0px;">
+	<td colspan="2" style="padding:10px 0px;">
 		<p style="font-style:oblique;">
 			<?php _e('Note: Nesting features not enabled for non-hierarchical post types.', 'nestedpages'); ?>
 		</p>
 	</td>
 </tr>
-<?php 
-//var_dump($this->getPostTypes());
-?>
