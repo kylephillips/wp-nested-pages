d877ecf4cc706aec8a2ea1c8d0ba9a851b8848a7
Kyle Phillips <kylephillipsdesign@gmail.com>
2015-01-04 16:12:04 -0500

    Bug fix in trashed post action - from change to storing visible pages in array by post type.

--- app/Entities/Post/PostActions.php
+++ app/Entities/Post/PostActions.php
@@ -1,11 +1,22 @@
 <?php namespace NestedPages\Entities\Post;
+
+use NestedPages\Entities\User\UserRepository;
+use NestedPages\Entities\NavMenu\NavMenuSyncListing;
+
 /**
 * WP Actions tied to a Post
 */
 class PostActions {
 
+	/**
+	* User Repository
+	* @var object
+	*/
+	private $user_repo;
+
 	public function __construct()
 	{
+		$this->user_repo = new UserRepository;
 		add_action( 'trashed_post', array( $this, 'trashHook' ) );
 	}
 
@@ -16,25 +27,35 @@ class PostActions {
 	public function trashHook($post_id)
 	{
 		$post_type = get_post_type($post_id);
-		if ( $post_type == 'page' ) $this->resetToggles($post_id);
+		if ( ($post_type == 'page') || ($post_type == 'np-redirect') ) {
+			$post_type = 'page';
+			$sync = new NavMenuSyncListing;
+			$sync->sync();
+		}
+		$this->resetToggles($post_id, $post_type);
 	}
 
 
 	/**
 	* Make sure children of trashed pages are viewable in Nested Pages
 	*/
-	private function resetToggles($post_id)
+	private function resetToggles($post_id, $post_type)
 	{
-		$visible_pages = unserialize(get_user_meta(get_current_user_id(), 'np_visible_pages', true));
+		$visible_pages = unserialize(get_user_meta(get_current_user_id(), 'np_visible_posts', true));
+		$visible_pages = $visible_pages[$post_type];
+		
 		$child_pages = array();
-		$children = new \WP_Query(array('post_type'=>'page', 'posts_per_page'=>-1, 'post_parent'=>$post_id));
+		
+		$children = new \WP_Query(array('post_type'=>$post_type, 'posts_per_page'=>-1, 'post_parent'=>$post_id));
 		if ( $children->have_posts() ) : while ( $children->have_posts() ) : $children->the_post();
 			array_push($child_pages, get_the_id());
 		endwhile; endif; wp_reset_postdata();
+		
 		foreach($child_pages as $child_page){
 			if ( !in_array($child_page, $visible_pages) ) array_push($visible_pages, $child_page);
 		}
-		update_user_meta(get_current_user_id(), 'np_visible_pages', serialize($visible_pages));
+
+		$this->user_repo->updateVisiblePages($post_type, $visible_pages);
 	}
 
 }
