c7024fdfec1559e0460b884d0daf1e3f5540279c
Ian Register <me@ianregister.com>
2015-09-18 19:13:34 +1000

    Fixed plugin directory constant definition

--- nestedpages.php
+++ nestedpages.php
@@ -47,7 +47,7 @@ function nestedpages_check_versions( $wp = '3.9', $php = '5.3.2' ) {
 }
 
 if( !class_exists('Bootstrap') ) :
-    define('NESTEDPAGES_DIR', __FILE__);
+    define('NESTEDPAGES_DIR', __DIR__);
     nestedpages_check_versions();
     require_once('vendor/autoload.php');
     require_once('app/NestedPages.php');
