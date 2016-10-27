11b0abdbdae4eaefd2271a8efc02032800b7c01e
Kyle Phillips <kylephillipsdesign@gmail.com>
2016-10-26 22:21:40 -0400

    Fix for duplicate/similar slug overrides in front end redirects.

--- app/RedirectsFrontEnd.php
+++ app/RedirectsFrontEnd.php
@@ -8,8 +8,8 @@ class RedirectsFrontEnd
 {
 	public function __construct()
 	{
-		// add_filter('page_link', array($this, 'pageLinks'),10,2);
-		// add_action('parse_request', array($this, 'parseRequest'));
+		add_filter('page_link', array($this, 'pageLinks'),10,2);
+		add_action('parse_request', array($this, 'parseRequest'));
 	}
 
 	/**
@@ -18,47 +18,46 @@ class RedirectsFrontEnd
 	public function pageLinks( $post_link, $id = 0 )
 	{
 		$post = get_post($id);
-		$post_link = $this->removeParentSlugs($post, $post_link);
+		$post_link = $this->renameLinkSlugs($post, $post_link);
 		return $post_link;
 	}
 
 	public function parseRequest($wp)
 	{
-
 		if ( isset($wp->query_vars['error']) ) $slug = basename($wp->request);
 		if ( isset($wp->query_vars['pagename']) && ! empty($wp->query_vars['pagename']) ) $slug = $wp->query_vars['pagename'];
 		if ( isset($wp->query_vars['name']) && ! empty($wp->query_vars['name']) ) $slug = $wp->query_vars['name'];
 		if ( isset($wp->query_vars['attachment']) && ! empty($wp->query_vars['attachment']) ) $slug = $wp->query_vars['attachment'];
-		if ( !isset($slug) ) return;		
+		if ( !isset($slug) ) return;	
 
-		$segments = explode('/', $slug);
+		$segments = explode('/', $wp->request);
 		$slug = basename($slug);
 
-		if ( count($segments) > 1 ){
-			$parent_slug = $segments[count($segments) - 2];
-			$parent_post = get_posts(array(
-				'name' => $parent_slug,
-				'post_type' => 'any',
-				'posts_per_page' => 1
-			));
+		$redirect = false;
+		if ( count($segments) == 1 ) return;
+		
+		$parent_slug = $segments[count($segments) - 2];
+		if ( substr($parent_slug, -4) == 'np-r' ){
+			$redirect = true;
+			$parent_slug = substr($parent_slug, 0, -5);
 		}
+		$parent_args = array(
+			'name' => $parent_slug,
+			'posts_per_page' => 1
+		);
+		$parent_args['post_type'] = ( $redirect ) ? 'np-redirect' : 'any';
+		$parent_post = get_posts($parent_args);
 
 		$page_args = array(
 			'name' => $slug, 
 			'post_type' => 'any', 
 			'posts_per_page' => 1
 		);
-		$page_args['post_parent'] = ( isset($parent_post) ) ? $parent_post[0]->ID : 0;
-		
+		$page_args['post_parent'] = ( isset($parent_post) && $redirect ) ? $parent_post[0]->ID : 0;
 		$page = get_posts($page_args);
+
 		if ( !$page ) return;
-
-		$parent_type = get_post_type($page[0]->post_parent);
-
-		if ( $parent_type !== 'np-redirect' && !isset($wp->query_vars['attachment']) && $parent_type !== 'page' ) return;
 		
-		unset($wp->query_vars['pagename']);
-		unset($wp->query_vars['name']);
 		unset($wp->query_vars['attachment']);
 		unset($wp->query_vars['error']);
 		
@@ -68,15 +67,15 @@ class RedirectsFrontEnd
 	/**
 	* Recursive function removes non-page parent slugs
 	*/
-	private function removeParentSlugs($post, $slug)
+	private function renameLinkSlugs($post, $slug)
 	{
 		if ( is_admin() ) return $slug;
 		if ( $post->post_parent > 0 ) {
 			$parent_post = get_post($post->post_parent);
 			if ( $parent_post->post_type == 'np-redirect' ){
-				$slug = str_replace($parent_post->post_name . '/', '', $slug);
+				$slug = str_replace($parent_post->post_name . '/', $parent_post->post_name . '-np-r/', $slug);
 			}
-			return $this->removeParentSlugs($parent_post, $slug);
+			return $this->renameLinkSlugs($parent_post, $slug);
 		}
 		return $slug;
 	}
