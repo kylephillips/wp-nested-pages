50d9fe6921dbe971f65c192626e89d959399eb10
Kyle Phillips <kyle@object9.com>
2017-05-09 08:21:30 -0400

    Various helper methods added to wpml integration class

--- app/Entities/PluginIntegration/WPML.php
+++ app/Entities/PluginIntegration/WPML.php
@@ -15,12 +15,58 @@ class WPML
 	*/
 	public $installed = false;
 
+	/**
+	* Global Sitepress object
+	*/
+	private $sitepress;
+
 	public function __construct()
 	{
 		if ( defined('ICL_SITEPRESS_VERSION') ){
 			$this->installed = true;
+			global $sitepress;
+			$this->sitepress = $sitepress;
 			return;
 		} 
+	}
+
+	/**
+	* Get the current language
+	* @return string
+	*/
+	public function getCurrentLanguage($return_type = 'code')
+	{
+		if ( $return_type == 'code' ) return ICL_LANGUAGE_CODE;
+		if ( $return_type == 'name' ) return ICL_LANGUAGE_NAME;
+		if ( $return_type == 'name_en' ) return ICL_LANGUAGE_NAME_EN;
+	}
+
+	/**
+	* Get all available languages
+	* @return array
+	*/
+	public function getLanguages()
+	{
+		return icl_get_languages();
+	}
+
+	/**
+	* Get the default language
+	* @return array
+	*/
+	public function getDefaultLanguage()
+	{
+		return $this->sitepress->get_default_language();
+	}
+
+	/**
+	* Get a single language
+	* @return array
+	*/
+	public function getSingleLanguage($language = 'en')
+	{
+		$languages = $this->getLanguages();
+		return ( array_key_exists($language, $languages) ) ? $languages[$language] : array();
 	}
 
 }
