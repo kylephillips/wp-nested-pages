70a1ae3c53294b86d9c4489c31435fd86ea18c2c
Kyle Phillips <kylephillipsdesign@gmail.com>
2017-06-07 20:18:48 -0400

    Fixes additional bugs in customizable sorting/filtering processing

--- app/Form/Listeners/ListingSort.php
+++ app/Form/Listeners/ListingSort.php
@@ -52,8 +52,7 @@ class ListingSort
 	private function setOrderBy()
 	{
 		$allowed = array('menu_order', 'date', 'title'); // prevent tomfoolery
-		if ( ($_POST['np_orderby'] !== "") && (!in_array($_POST['np_orderby'], $allowed)) ) $this->url .= '&orderby=menu_order';
-		$this->url .= '&orderby=' . sanitize_text_field($_POST['np_orderby']);
+		if ( isset($_POST['np_orderby']) && $_POST['np_orderby'] !== "" && in_array($_POST['np_orderby'], $allowed) ) $this->url .= '&orderby=' . sanitize_text_field($_POST['np_orderby']);
 	}
 
 	/**
@@ -62,8 +61,7 @@ class ListingSort
 	private function setOrder()
 	{
 		$allowed = array('ASC', 'DESC'); // prevent tomfoolery
-		if ( !in_array($_POST['np_order'], $allowed) ) $this->url .= '&order=DESC';
-		$this->url .= '&order=' . sanitize_text_field($_POST['np_order']);
+		if ( isset($_POST['np_order']) && in_array($_POST['np_order'], $allowed) ) $this->url .= '&order=' . sanitize_text_field($_POST['np_order']);
 	}
 
 	/**
@@ -71,9 +69,7 @@ class ListingSort
 	*/
 	private function setAuthor()
 	{
-		if ( (isset($_POST['np_author'])) && ($_POST['np_author'] !== "") ){
-			$this->url .= '&author=' . sanitize_text_field($_POST['np_author']);
-		}
+		if ( (isset($_POST['np_author'])) && ($_POST['np_author'] !== "") )	$this->url .= '&author=' . sanitize_text_field($_POST['np_author']);
 	}
 
 	/**
