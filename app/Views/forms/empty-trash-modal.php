3f9e1c9437cc3e448ea1f16cde1422d3b483a7ce
Aurélien Joahny <aurelien@unikmedia.fr>
2018-02-12 10:26:23 +0100

    Adds an untranslated string to translation

--- app/Views/forms/empty-trash-modal.php
+++ app/Views/forms/empty-trash-modal.php
@@ -7,7 +7,7 @@
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-body">
-				<p>Are you sure you would like to empty the trash? This action is not reversable.</p>
+				<p><?php _e('Are you sure you would like to empty the trash? This action is not reversable.', 'wp-nested-pages'); ?></p>
 				<a href="#" class="np-cancel-trash button modal-close" data-dismiss="modal"><?php _e('Cancel', 'wp-nested-pages'); ?></a>
 				<a href="#" class="np-trash-confirm button-primary"><?php _e('Empty Trash', 'wp-nested-pages'); ?></a>
 				<input type="hidden" id="np-trash-posttype" value="<?php echo esc_attr($this->post_type->name); ?>">
