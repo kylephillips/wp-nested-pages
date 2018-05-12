0372f901c3a1df3538b8f218f0a339e794e59e3a
Kyle Phillips <kylephillipsdesign@gmail.com>
2018-05-12 17:49:48 -0400

    Adds svg link icon to Link quick edit title population

--- assets/js/nestedpages.js
+++ assets/js/nestedpages.js
@@ -2101,9 +2101,8 @@ NestedPages.QuickEditLink = function()
 	// Update the row after successfully saving quick edit data
 	plugin.updateRow = function()
 	{
-		console.log(plugin.newPostData);
 		var row = $(plugin.form).siblings('.row');
-		$(row).find('.title').html(plugin.newPostData.post_title + ' <i class="np-icon-link"></i>');
+		$(row).find('.title').html(plugin.newPostData.post_title + ' <svg class="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path class="icon" d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>');
 		
 		var status = $(row).find('.status');
 		if ( (plugin.newPostData._status !== 'publish') && (plugin.newPostData._status !== 'future') ){
