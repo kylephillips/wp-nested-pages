18d421d87870d8c9fb29c9fafa0581d4f0a5a8f3
Kyle Phillips <kylephillipsdesign@gmail.com>
2018-05-12 17:28:49 -0400

    Replaces handle icon in new post modal with inline svg

--- app/Views/forms/new-child.php
+++ app/Views/forms/new-child.php
@@ -14,7 +14,7 @@
 
 		<ol class="new-page-titles">
 			<li>
-				<i class="handle np-icon-menu"></i>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="handle np-icon-menu"><path d="M0 0h24v24H0z" fill="none" /><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="bars" /></svg>
 				<div class="form-control new-child-row">
 					<label><?php _e( 'Title' ); ?></label>
 					<div>
--- assets/js/lib/nestedpages.new-post.js
+++ assets/js/lib/nestedpages.new-post.js
@@ -100,7 +100,7 @@ NestedPages.NewPost = function()
 	{		
 		var form = $(button).parents('form');
 		var fieldcount = $(button).siblings('.new-page-titles').children('li').length + 1;
-		var html = '<li><i class="handle np-icon-menu"></i><div class="form-control new-child-row"><label>' + NestedPages.jsData.titleText + '</label><div><input type="text" name="post_title[]" class="np_title" placeholder="' + NestedPages.jsData.titleText + '" value="" tabindex="' + fieldcount + '" /><a href="#" class="button-secondary np-remove-child">-</a></div></div></li>';
+		var html = '<li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="handle np-icon-menu"><path d="M0 0h24v24H0z" fill="none" /><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" class="bars" /></svg><div class="form-control new-child-row"><label>' + NestedPages.jsData.titleText + '</label><div><input type="text" name="post_title[]" class="np_title" placeholder="' + NestedPages.jsData.titleText + '" value="" tabindex="' + fieldcount + '" /><a href="#" class="button-secondary np-remove-child">-</a></div></div></li>';
 		var container = $(button).siblings('.new-page-titles').append(html);
 		$(form).find('.np_title').last().focus();
 		$(form).find(NestedPages.selectors.newPageTitle).prop('tabindex', fieldcount++);
