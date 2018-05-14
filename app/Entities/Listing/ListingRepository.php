2db6817f83c9252abff45f67e8a7312d7f68672c
Kyle Phillips <kylephillipsdesign@gmail.com>
2018-05-13 21:04:53 -0400

    Fixes issue where corrupted visible pages meta value was preventing page listing from being displayed

--- app/Entities/Listing/ListingRepository.php
+++ app/Entities/Listing/ListingRepository.php
@@ -21,7 +21,7 @@ class ListingRepository
 	public function visiblePages($post_type)
 	{
 		$visible = unserialize(get_user_meta(get_current_user_id(), 'np_visible_posts', true));
-		if ( !isset($visible[$post_type]) ) $visible[$post_type] = [];
+		if ( !isset($visible[$post_type]) || !$visible ) $visible[$post_type] = [];
 		return $visible[$post_type];
 	}
 
