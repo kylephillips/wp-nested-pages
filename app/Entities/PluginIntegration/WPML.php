c5332892efba345a1dc6940ab729a17ddff97a1b
Kyle Phillips <kyle@object9.com>
2020-05-22 13:36:08 -0400

    Fixes ajax php notice when synching order of WPML translations

--- app/Entities/PluginIntegration/WPML.php
+++ app/Entities/PluginIntegration/WPML.php
@@ -130,7 +130,8 @@ class WPML
 			$translations = $this->getAllTranslations($post['id']);
 			foreach ( $translations as $lang_code => $post_info ) :
 				$translation_post_id = $post_info->element_id;
-				$translated_parent = $this->getAllTranslations($post_parent)[$lang_code]->element_id;
+				$parent_translations = $this->getAllTranslations($post_parent);
+				$translated_parent = ( isset($parent_translations[$lang_code]) ) ? $parent_translations[$lang_code]->element_id : 0;
 				$query = "UPDATE $wpdb->posts SET menu_order = '$order', post_parent = '$translated_parent' WHERE ID = '$translation_post_id'";
 				$wpdb->query( $query );
 			endforeach;
