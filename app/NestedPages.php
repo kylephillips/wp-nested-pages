c58e7328f063f40f3412f07e60391e67838f08f4
Kyle Phillips <kyle@object9.com>
2020-05-22 11:24:04 -0400

    Updates plugin versions

--- app/NestedPages.php
+++ app/NestedPages.php
@@ -12,7 +12,7 @@ class NestedPages
 		$np_env = 'live';
 
 		global $np_version;
-		$np_version = '3.1.11';
+		$np_version = '3.1.12';
 
 		if ( is_admin() ) $app = new NestedPages\Bootstrap;
 		if ( !is_admin() ) $app = new NestedPages\FrontEndBootstrap;
