d21c7c2b0a9b1d8d214c6a18d089d7fe5c894270
Kyle Phillips <kyle@object9.com>
2021-10-11 15:35:33 -0400

    Corrects invalid sprint parameter. Closes #312

--- app/Views/partials/row.php
+++ app/Views/partials/row.php
@@ -128,7 +128,7 @@ if ( !$wpml ) $wpml_pages = true;
 					<li>
 						<a href="<?php echo admin_url( 'edit-comments.php?p=' . get_the_id() ); ?>">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="primary" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-						<?php echo sprintf(__('%i Comments', 'wp-nested-pages'), intval($comments->total_comments)); ?>
+						<?php echo sprintf(__('%d Comments', 'wp-nested-pages'), intval($comments->total_comments)); ?>
 						</a>
 					</li>
 					<?php else : $cs = 'closed'; endif; ?>
